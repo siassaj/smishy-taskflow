08264db3dd9022a55f5e9c99443d011793edb374
Swishy <super.quazimodo@gmail.com>
2013-05-23 15:17:46 +1000

    Prepare smishy taskflow for git-flow and public git repo

---
+++ smishy-taskflow-open.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##################### Script to pop open into smishy-taskflow ######################
+# Bind this script to a keystroke in your wm, it will pop into the existing
+# smishy-taskflow instance, or else it will create one if it doesnt exist :D
+#
+#######################################  ########################################
+
+#Test to see if smishy-taskflow socket exists
+
+config_location="/home/quazimodo/Code/smishy-taskflow/.smishy-taskflow"
+
+if [[ `screen -ls | grep -owc "smishy-taskflow"` = 1 ]]; then
+    xterm -fa monaco -fs 12 -geometry 80x74 -e screen -raAd smishy-taskflow
+else
+    screen -dmS smishy-taskflow emacs -nw -q -l $config_location
+fi
