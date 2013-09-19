36485118239f2d906fd8bca31624452e335befa2
Swishy <super.quazimodo@gmail.com>
2013-09-19 11:57:38 +1000

    Basic live updates are working. Clock table is not

--- smishy-taskflow.el
+++ smishy-taskflow.el
@@ -1,6 +1,6 @@
 ;;;; -*- mode:emacs-lisp;coding:utf-8 -*-
 ;;;;**************************************************************************
-;;;;FILE:               smishy-taskflow.el
+;;;;FILE:               smishy--taskflow.el
 ;;;;LANGUAGE:           emacs lisp
 ;;;;SYSTEM:             emacs
 ;;;;USER-INTERFACE:     emacs
@@ -22,7 +22,7 @@
 ;;;;    Copyright Siavash S. Sajjadi 2011 - 2013
 ;;;;
 ;;;;    This library is free software; you can redistribute it and/or
-n;;;;    modify it under the terms of the GNU Lesser General Public
+;;;;    modify it under the terms of the GNU Lesser General Public
 ;;;;    License as published by the Free Software Foundation; either
 ;;;;    version 3 of the License, or (at your option) any later
 ;;;;    version.
@@ -41,27 +41,27 @@ n;;;;    modify it under the terms of the GNU Lesser General Public
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;; Make sure to have dependencies ;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require 'screen)
 (require 'org)
 (require 'popup)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Define Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun smishy-kill-other-buffers ()
+(defun smishy--kill-other-buffers ()
   "Kill all other buffers."
   (interactive)
   (mapc 'kill-buffer
         (delq (current-buffer)
               (remove-if-not 'buffer-file-name (buffer-list)))))
 
-(defun smishy-reload-top ()
+(defun smishy--reload-top ()
   "Prepare new Next Action line.
-This function will jump to line smishy-work-line, newline it and then put a * NEXT ACTION line in and get ready for input."
+This function will jump to line smishy--work-line, newline it and then put a * NEXT ACTION line in and get ready for input."
   (interactive)
   ;; (switch-to-buffer "biglist.org")
   (delete-other-windows)
-  (goto-line smishy-work-line)
+  (goto-line smishy--work-line)
   (setq mystr (buffer-substring (point-at-bol) (point-at-eol)))
   (cond ((string= mystr "* NEXT ACTION ") (move-end-of-line 1))
         ((string= mystr "* NEXT ACTION")
@@ -72,42 +72,42 @@ This function will jump to line smishy-work-line, newline it and then put a * NE
          (move-beginning-of-line 1)
          (kill-line)
          (newline)
-         (goto-line smishy-work-line)
+         (goto-line smishy--work-line)
          (insert "* NEXT ACTION "))
         ((string-match "^ +$" mystr) ;tests for 1 or more blank spaces only
          (move-beginning-of-line 1)
          (kill-line)
          (newline)
-         (goto-line smishy-work-line)
+         (goto-line smishy--work-line)
          (insert "* NEXT ACTION "))
         (t
          (move-beginning-of-line 1)
          (newline)
-         (goto-line smishy-work-line)
+         (goto-line smishy--work-line)
          (insert "* NEXT ACTION ")))
-  (goto-line (+ 1 smishy-work-line))
+  (goto-line (+ 1 smishy--work-line))
   (if (string-match "\* NEXT ACTION ." mystr)
       (org-todo "TODO"))
   (setq mystr2 (buffer-substring (point-at-bol) (point-at-eol)))
-  (goto-line (+ 2 smishy-work-line))
+  (goto-line (+ 2 smishy--work-line))
   ;; This bit processes the 2nd line under the work line and turns it into a
   ;; TODO which doesn't really seem to be a good thing
   ;; (if (and (string-match "\* TODO " mystr2)
   ;;          (string-match "\* NEXT ACTION ." mystr))
   ;;     (org-todo "TODO"))
-  (goto-line smishy-work-line)
+  (goto-line smishy--work-line)
   (move-end-of-line 1)
   (save-buffer))
 
-(defun smishy-save-n-go ()
+(defun smishy--save-n-go ()
   "Save new Next Action and clone frame.
 Push the current task down, add a new DOING, then save the whole file. Finally, detach the screen, thus killing the xterm."
   (interactive)
-  (smishy-reload-top)
+  (smishy--reload-top)
   (save-buffer)
-  (shell-command "screen -D smishy-taskflow"))
+  (shell-command "screen -D smishy--taskflow"))
 
-(defun smishy-toggle-done ()
+(defun smishy--toggle-done ()
   "Toggle TODO/DONE on current line.
 Finish task on the current line and save it at the bottom as 'DONE'"
   (interactive)
@@ -115,7 +115,7 @@ Finish task on the current line and save it at the bottom as 'DONE'"
   (cond ((string-match "^\** TODO " mystr) (org-todo "DONE"))
         ((string-match "^\** DONE " mystr) (org-todo "TODO"))))
 
-(defun smishy-set-max-priority()
+(defun smishy--set-max-priority()
   "Set the current TODO to have the maximum priority"
   (let* ((marker (or (org-get-at-bol 'org-hd-marker)
                      (org-agenda-error)))
@@ -128,6 +128,40 @@ Finish task on the current line and save it at the bottom as 'DONE'"
           (org-back-to-heading t)
           (org-priority 65))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;; live update agenda view ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun smishy--run-agenda-cmd (f)
+  "Run commands \"f\" in Agenda buffer. You can get these
+commands using \"smishy--get-org-agenda-view-commands\"."
+  (save-window-excursion
+    (let* ((line (org-current-line)))
+      (if f (eval f))
+      (org-goto-line line))))
+
+(defun smishy--get-org-agenda-view-commands ()
+  "Get commands by which the current state of Agenda buffer can
+be restored using \"(eval commands)\"."
+  (let* ((p (or (and (looking-at "\\'") (1- (point))) (point)))
+         (series-redo-cmd (get-text-property p 'org-series-redo-cmd)))
+    (if series-redo-cmd
+      (get-text-property p 'org-series-redo-cmd)
+      (get-text-property p 'org-redo-cmd))))
+
+(defun smishy--auto-update-agenda (x y z)
+  (interactive)
+  (dolist (buf (smishy--get-agenda-buffers))
+    (with-current-buffer buf
+      (smishy--run-agenda-cmd (smishy--get-org-agenda-view-commands)))))
+
+(defun smishy--set-after-change-hook (buf)
+  (with-current-buffer buf (add-hook 'after-change-functions 'smishy--auto-update-agenda nil buf)))
+
+(defun smishy--set-after-major-mode-change-hook ()
+  (add-hook 'after-change-major-mode-hook
+            (lambda ()
+              (when (eq major-mode 'org-mode)
+                (smishy--set-after-change-hook (current-buffer))))))
+
 (defun smishy--get-agenda-buffers ()
   (let (blist)
     (dolist (buf (buffer-list))
@@ -135,75 +169,20 @@ Finish task on the current line and save it at the bottom as 'DONE'"
         (push buf blist)))
     blist))
 
-(defun smishy-auto-update-agenda (x y z)
-  (interactive)
-  (dolist (buf (smishy--get-agenda-buffers))
-    (with-current-buffer buf (smishy--org-agenda-redo))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; set variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun smishy-set-after-major-mode-change-hook ()
-  (add-hook 'after-change-major-mode-hook
-            (lambda ()
-              (when (eq major-mode 'org-mode)
-                (smishy-set-after-change-hook (current-buffer))))))
-
-(defun smishy-set-after-change-hook (buf)
-  (with-current-buffer buf (add-hook 'after-change-functions 'smishy-auto-update-agenda nil buf)))
-
-(defun smishy--org-agenda-redo (&optional all)
-  (let* ((p (or (and (looking-at "\\'") (1- (point))) (point)))
-         (cpa (unless (eq all t) current-prefix-arg))
-         (org-agenda-doing-sticky-redo org-agenda-sticky)
-         (org-agenda-sticky nil)
-         (org-agenda-buffer-name (or org-agenda-this-buffer-name
-                                     org-agenda-buffer-name))
-         (org-agenda-keep-modes t)
-         (tag-filter org-agenda-tag-filter)
-         (tag-preset (get 'org-agenda-tag-filter :preset-filter))
-         (top-cat-filter org-agenda-top-category-filter)
-         (cat-filter org-agenda-category-filter)
-         (cat-preset (get 'org-agenda-category-filter :preset-filter))
-         (org-agenda-tag-filter-while-redo (or tag-filter tag-preset))
-         (cols org-agenda-columns-active)
-         (line (org-current-line))
-         (window-line (- line (org-current-line (window-start))))
-         (lprops (get 'org-agenda-redo-command 'org-lprops))
-         (redo-cmd (get-text-property p 'org-redo-cmd))
-         (last-args (get-text-property p 'org-last-args))
-         (org-agenda-overriding-cmd (get-text-property p 'org-series-cmd))
-         (org-agenda-overriding-cmd-arguments
-          (unless (eq all t)
-            (cond ((listp last-args)
-                   (cons (or cpa (car last-args)) (cdr last-args)))
-                  ((stringp last-args)
-                   last-args))))
-         (series-redo-cmd (get-text-property p 'org-series-redo-cmd)))
-    (put 'org-agenda-tag-filter :preset-filter nil)
-    (put 'org-agenda-category-filter :preset-filter nil)
-    (and cols (org-columns-quit))
-    (if series-redo-cmd
-        (eval series-redo-cmd)
-      (org-let lprops '(eval redo-cmd)))
-    (setq org-agenda-undo-list nil
-          org-agenda-pending-undo-list nil)
-    (put 'org-agenda-tag-filter :preset-filter tag-preset)
-    (put 'org-agenda-category-filter :preset-filter cat-preset)
-    (and (or tag-filter tag-preset) (org-agenda-filter-apply tag-filter 'tag))
-    (and (or cat-filter cat-preset) (org-agenda-filter-apply cat-filter 'category))
-    (and top-cat-filter (org-agenda-filter-top-category-apply top-cat-filter))
-    (and cols (org-called-interactively-p 'any) (org-agenda-columns))
-    (org-goto-line line)))
-
-(defun smishy-set-variables ()
-  "Set important smishy-taskflow variables."
-  (setq smishy-work-line 9) ;set what line you will be entering your task
-  (setq org-agenda-start-with-clockreport-mode t)
+(defun smishy--set-variables ()
+  "Set important smishy--taskflow variables."
+  (setq smishy--work-line 9) ;set what line you will be entering your task
+  (setq org-agenda-start-with-clockreport-mode nil)
   (setq org-agenda-custom-commands
         (append org-agenda-custom-commands
                 '(("h" "2 week agenda, DONE and TODOs"
                    ((agenda "" ((org-agenda-start-on-weekday nil)))
                     (todo "DONE")
                     (todo "DELETED")
-                    (todo "TODO"))))))
+                    (todo "TODO"))
+                   ((org-agenda-compact-blocks t))))))
   (setq org-agenda-start-with-follow-mode t)
   (setq org-lowest-priority 69)
   (setq org-default-priority 68)
@@ -211,21 +190,8 @@ Finish task on the current line and save it at the bottom as 'DONE'"
   (setq org-startup-indented t)
   (setq org-startup-folded 'content))
 
-;; #458b00
-;; #66cd00
-;; #76ee00
-;; #7fff00
-;; #008b00
-;; #00cd00
-;; #00ee00
-;; #00ff00
-;; #008b45
-;; #00cd66
-;; #00ee76
-;; #00ff7f
-
-(defun smishy-set-faces ()
-  "Set smishy-taskflow faces."
+(defun smishy--set-faces ()
+  "Set smishy--taskflow faces."
   (setq org-todo-keywords '((type "NEXT ACTION" "DOING" "TODO" "PROJECT" "DEFERRED" "DELEGATED" "REF" "NOTE" "|" "DONE" "DELETED")))
   ;; Use hex values for terminal and gui color support
   (setq org-todo-keyword-faces
@@ -249,30 +215,30 @@ Finish task on the current line and save it at the bottom as 'DONE'"
    '(org-level-4 ((t (:foreground "#d7ff87"))))
    '(org-special-keyword ((t (:foreground "#ff00ff" :background "#00005f"))))))
 
-(defun smishy-set-key-bindings ()
-  "Set smishy-taskflow key-bindings.
+(defun smishy--set-key-bindings ()
+  "Set smishy--taskflow key-bindings.
 Sets keys for org-mode-map and org-agenda-mode-map.
    ',.p y|f gcrl
    aoeu i|d htns
     qjk x|b mwvz "
-  (define-key org-mode-map (kbd "C-c C-c") 'smishy-insert-todo)
+  (define-key org-mode-map (kbd "C-c C-c") 'smishy--insert-todo)
   (define-key org-mode-map (kbd "C-c C-d") 'org-deadline)
   (define-key org-mode-map (kbd "C-c C-h") 'org-schedule)
   (define-key org-mode-map (kbd "C-c C-t") 'org-set-tags)
   (define-key org-mode-map (kbd "C-c C-l") 'org-store-link)
 
-  (define-key org-mode-map (kbd "C-c e") 'smishy-reload-tasks)
-  (define-key org-mode-map (kbd "C-c p") 'smishy-create-project)
-  (define-key org-mode-map (kbd "C-c c") 'smishy-reload-top)
+  (define-key org-mode-map (kbd "C-c e") 'smishy--reload-tasks)
+  (define-key org-mode-map (kbd "C-c p") 'smishy--create-project)
+  (define-key org-mode-map (kbd "C-c c") 'smishy--reload-top)
   (define-key org-mode-map (kbd "C-c r") 'org-clock-goto)
   (define-key org-mode-map (kbd "C-c a") 'org-agenda)
   (define-key org-mode-map (kbd "C-c o") 'org-clock-out)
   (define-key org-mode-map (kbd "C-c i") 'org-clock-in)
-  (define-key org-mode-map (kbd "C-c d") 'smishy-toggle-done)
+  (define-key org-mode-map (kbd "C-c d") 'smishy--toggle-done)
   (define-key org-mode-map (kbd "C-c h") (lambda () (interactive) (org-agenda nil "h")))
   (define-key org-mode-map (kbd "C-c t") (lambda () (interactive) (org-todo-list "TODO")))
   (define-key org-mode-map (kbd "C-c n") (lambda () (interactive) (org-agenda-list 56)))
-  (define-key org-mode-map (kbd "C-c s") 'smishy-save-n-go)
+  (define-key org-mode-map (kbd "C-c s") 'smishy--save-n-go)
   (define-key org-mode-map (kbd "C-c b") 'org-iswitchb)
 
   ;; The following keybinds are for when emacs is in an xterm, shift + direction
@@ -281,9 +247,9 @@ Sets keys for org-mode-map and org-agenda-mode-map.
   (define-key org-mode-map (kbd "ESC [ 1 ; 2 C") (kbd "<S-right>"))
   (define-key org-mode-map (kbd "ESC [ 1 ; 2 A") (kbd "<S-up>"))
   (define-key org-mode-map (kbd "ESC [ 1 ; 2 B") (kbd "<S-down>"))
-  (define-key org-agenda-mode-map (kbd "TAB") 'smishy-tab-out-of-agenda))
+  (define-key org-agenda-mode-map (kbd "TAB") 'smishy--tab-out-of-agenda))
 
-(defun smishy-tab-out-of-agenda (&optional highlight)
+(defun smishy--tab-out-of-agenda (&optional highlight)
   "Tab out of agenda view easily.
 Used to tab out of the agenda view when the marker is not on a todo, which org-mode usually responds to by throwing an error."
   (interactive)
@@ -293,7 +259,7 @@ Used to tab out of the agenda view when the marker is not on a todo, which org-m
 
 (defun smishy--tab-out-of-agenda-list ()
   "Create popup list to tab out of agenda view.
-Usually called by smishy-tab-out-of-agenda, this function either creates a list of org files that org-agenda knows about, or tabs out to it if only 1 is known."
+Usually called by smishy--tab-out-of-agenda, this function either creates a list of org files that org-agenda knows about, or tabs out to it if only 1 is known."
   (let* ((file-path (cond ((equal 1 (length org-agenda-files))
                            (car org-agenda-files))
                           ((< 1 (length org-agenda-files))
@@ -303,16 +269,16 @@ Usually called by smishy-tab-out-of-agenda, this function either creates a list 
       (switch-to-buffer-other-window buffer)
       (find-file-other-window file-path))))
 
-(defun smishy-create-project ()
+(defun smishy--create-project ()
   "Create an Org-Mode project.
 Creates an Org-Mode project, then allows you to insert a TODO"
   (interactive)
-  (if (equal (line-number-at-pos) smishy-work-line)
-    (goto-line (+ smishy-work-line 1)))
+  (if (equal (line-number-at-pos) smishy--work-line)
+    (goto-line (+ smishy--work-line 1)))
   (org-insert-todo-heading nil)
   (org-todo "PROJECT"))
 
-(defun smishy-insert-todo ()
+(defun smishy--insert-todo ()
   "Insert TODO under current header.
 Creates a TODO nested under the current header at the current line"
   (interactive)
@@ -321,6 +287,8 @@ Creates a TODO nested under the current header at the current line"
   (org-insert-todo-heading nil)
   (org-todo "TODO"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; public functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun smishy-start-taskflow (file-path)
   "Start the smishy task flow"
   (interactive)
@@ -328,39 +296,57 @@ Creates a TODO nested under the current header at the current line"
   (let ((buff (find-file-noselect file-path)))
     (pop-to-buffer buff)
     (add-to-list 'org-agenda-files (buffer-file-name buff)))
-  (smishy-set-variables)
-  (smishy-set-faces)
-  (smishy-set-key-bindings)
-  ;; (smishy-set-after-major-mode-change-hook)
-  (smishy-reload-top)
+  (smishy--set-variables)
+  (smishy--set-faces)
+  (smishy--set-key-bindings)
+  (smishy--set-after-major-mode-change-hook)
+  (smishy--reload-top)
   (org-mode))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Test stuff, Ignore! ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun smishy-lock-spaces-down ()
+
+(defun smishy--lock-spaces-down ()
   "This function makes the entire document read only, except for a space on
 the work line. It is problematic as I still cant apply functions properly..."
   (interactive)
   ;; find the point that indicates the end of the 1st read only region
-  (goto-line smishy-work-line)
+  (goto-line smishy--work-line)
   (setq mystr (buffer-substring (point-at-bol) (point-at-eol)))
   (if (string-match "\* [A-Z]+ " mystr)
       (progn
         (setq readonly-region1-end (- (+ (point) (match-end 0)) 1)))
     (setq readonly-region1-end nil))
   ;; find the point that indicates the start of the 2nd read only region
-  (goto-line (+ smishy-work-line 1))
+  (goto-line (+ smishy--work-line 1))
   (setq readonly-region2-start (- (point) 1))
   ;; set read only regions
   (if readonly-region1-end
       (add-text-properties (point-min) readonly-region1-end '(read-only t)))
   (add-text-properties readonly-region2-start (point-max) '(read-only t)))
 
-(defun smishy-reload-tasks ()
-  "Function to reload .smishy-taskflow, used during devel"
+(defun smishy--reload-tasks ()
+  "Function to reload .smishy--taskflow, used during devel"
   (interactive)
-  (load-file "~/Code/smishy-taskflow/smishy-taskflow.el"))
+  (load-file "~/Code/smishy--taskflow/smishy--taskflow.el"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;; Fancy colors, Ignore! ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; #458b00
+;; #66cd00
+;; #76ee00
+;; #7fff00
+;; #008b00
+;; #00cd00
+;; #00ee00
+;; #00ff00
+;; #008b45
+;; #00cd66
+;; #00ee76
+;; #00ff7f
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Finished ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (provide 'smishy-taskflow)
+
+
+;; (setq org-agenda-start-with-clockreport-mode nil)
