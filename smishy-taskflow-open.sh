5f8a8e03645aacd20683fbc35339e2c8774d4eb9
Swishy <super.quazimodo@gmail.com>
2014-01-11 19:58:07 +1100

    Small update

--- smishy-taskflow-open.sh
+++ smishy-taskflow-open.sh
@@ -6,10 +6,10 @@
 #SYSTEM:             POSIX
 #USER-INTERFACE:     NONE
 #DESCRIPTION
-#    
+#
 #    Bind this script to a keystroke in your wm, it will pop into the existing
 #    smishy-taskflow instance, or else it will create one if it doesnt exist.
-#    
+#
 #AUTHORS
 #    <quazimodo> Siavash S. Sajjadi <super.quazimodo@gmail.com>
 #MODIFICATIONS
@@ -17,21 +17,21 @@
 #BUGS
 #LEGAL
 #    LGPL3
-#    
+#
 #    Copyright Siavash S. Sajjadi 2011 - 2013
-#    
+#
 #    This library is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU Lesser General Public
 #    License as published by the Free Software Foundation; either
 #    version 3 of the License, or (at your option) any later
 #    version.
-#    
+#
 #    This library is distributed in the hope that it will be
 #    useful, but WITHOUT ANY WARRANTY; without even the implied
 #    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 #    PURPOSE.  See the GNU Lesser General Public License for more
 #    details.
-#    
+#
 #    You should have received a copy of the  GNU Lesser General
 #    Public License along with this library.
 #    If not, see <http://www.gnu.org/licenses/>.
@@ -42,7 +42,9 @@
 config_location="~/.emacs.d/smishy-taskflow-init.el"
 
 if [[ `screen -ls | grep -owc "smishy-taskflow"` = 1 ]]; then
-    xterm -fa monaco -fs 12 -geometry 80x74 -e screen -raAd smishy-taskflow
+    # ATTACH
+    xterm -Te screen -raAd smishy-taskflow
 else
+    # CREATE
     screen -dmS smishy-taskflow emacs -nw -q -l $config_location
 fi
