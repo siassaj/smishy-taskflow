7b5ec13d1655a4ab189595437d08ad37056dc266
Swishy <super.quazimodo@gmail.com>
2014-03-28 22:14:37 +1100

    Move away from smishy ideas, since many suck.
    Use org-capture instead of a dedicated insert line
    Completely remap keys
    Configure TODO states to be more GTD like
    Use tags in org-capture templates for GTD action contexts
    Add new org-agenda custom commands

--- configure.el
+++ configure.el
@@ -2,33 +2,56 @@
   "Set important smishy--taskflow variables."
   (setq smishy--work-line 9) ;set what line you will be entering your task
   (setq org-agenda-start-with-clockreport-mode t)
+  (setq org-tag-alist '("general(g)" "home(h)" "call(c)" "mail(m)" "errand(e)" "event(E)" "review(r)"))
   (setq org-agenda-custom-commands
-        '(("h" "2 week agenda, DONE and TODOs"
+        '(("g" tags-todo "general")
+          ("c" tags-todo "call")
+          ("m" tags-todo "mail")
+          ("e" tags-todo "errand")
+          ("E" tags-todo "event")
+          ("r" tags-todo "review")
+          ("H" tags-todo "home")
+          ("h" "2 week agenda, DONE and ACTIONs"
            ((agenda "" ((org-agenda-start-on-weekday nil)))
             (todo "DONE")
             (todo "DELETED")
-            (todo "TODO")
-            (stuck ""))
+            (todo "ACTION")
+            (stuck "PROJECT"))
            ((org-agenda-compact-blocks nil)))))
   (setq org-agenda-start-with-follow-mode t)
   (setq org-lowest-priority 69)
   (setq org-default-priority 68)
-  (setq org-stuck-projects '("/PROJECT" ("TODO") nil ""))
-  ;; (setq org-stuck-projects '("{^.*}/PROJECT" ("TODO" "DOING") nil ""))
+  (setq org-stuck-projects '("/PROJECT" ("ACTION") nil ""))
   (setq org-startup-indented t)
   (setq org-startup-folded 'content))
 
+(defun smishy--set-capture-templates ()
+  (setq org-capture-templates
+        '(("g" "General" entry (file "~/org/biglist.org")
+           "* ACTION %? :general:\n%u")
+          ("h" "Home" entry (file "~/org/biglist.org")
+           "* ACTION %? :home:\n%u")
+          ("c" "Call" entry (file "~/org/biglist.org")
+           "* ACTION %? :call:\n%u")
+          ("m" "(e)Mail" entry (file "~/org/biglist.org")
+           "* ACTION %? :mail:\n%u")
+          ("e" "Errand" entry (file "~/org/biglist.org")
+           "* ACTION %? :errand:\n%u")
+          ("E" "Event" entry (file "~/org/biglist.org")
+           "* ACTION %? :event:\n%u")
+          ("r" "Read/Review" entry (file "~/org/biglist.org")
+           "* ACTION %? :review:\n%u"))))
+
 (defun smishy--set-faces ()
   "Set smishy--taskflow faces."
-  (setq org-todo-keywords '((type "NEXT ACTION" "DOING" "TODO" "PROJECT" "DEFERRED" "DELEGATED" "REF" "NOTE" "|" "DONE" "DELETED")))
+  (setq org-todo-keywords '((type "ACTION" "PROJECT" "SOMEDAY" "WAITING" "|" "DONE" "DELETED")
+                            (type "|" "REF" "NOTE")))
   ;; Use hex values for terminal and gui color support
   (setq org-todo-keyword-faces
-        '(("NEXT ACTION" . (:foreground "#87d7d7" :background "#005c5c"))
-          ("DOING" . (:foreground "#5c5c00" :background "#d7ff5f"))
-          ("TODO" . (:foreground "#eeeeee" :background "#9a32cd"))
+        '(("ACTION" . (:foreground "#eeeeee" :background "#9a32cd"))
           ("PROJECT" . (:foreground "#005c00" :background "#5fff5f"))
-          ("DEFERRED" . (:foreground "#444444" :background "#d7875f"))
-          ("DELEGATED" . (:foreground "#005c5c" :background "#87d7d7"))
+          ("SOMEDAY" . (:foreground "#444444" :background "#d7875f"))
+          ("WAITING" . (:foreground "#005c5c" :background "#87d7d7"))
           ("REF" . (:foreground "#333333" :background "#bebebe"))
           ("NOTE" . (:foreground "#870000" :background "#af005f"))
           ("DONE" . (:foreground "#5f0000" :background "#eeeeee"))
@@ -36,12 +59,13 @@
   (custom-set-faces
 
    '(default ((t (:background "#242424"))))
-   '(org-tag ((t (:foreground "#000000" :background "#5fff00"))))
+   '(org-tag ((t (:foreground "#555555"))))
    '(org-level-1 ((t (:foreground "#d7ffff"))))
    '(org-level-2 ((t (:foreground "#d7ffd7"))))
    '(org-level-3 ((t (:foreground "#d7ffaf"))))
    '(org-level-4 ((t (:foreground "#d7ff87"))))
    '(org-special-keyword ((t (:foreground "#ff00ff" :background "#00005f"))))))
+
 
 (defun smishy--set-key-bindings ()
   "Set smishy--taskflow key-bindings.
@@ -51,21 +75,7 @@ Sets keys for org-mode-map and org-agenda-mode-map.
     qjk x|b mwvz "
   (eval-after-load "org"
     '(progn
-       (define-key org-mode-map (kbd "C-t") nil)
-       (define-key org-mode-map (kbd "C-t M-t") 'smishy-insert-new-heading-todo)
-       (define-key org-mode-map (kbd "C-t M-p") 'smishy-insert-new-heading-project)
-       (define-key org-mode-map (kbd "C-t M-n") 'smishy-insert-new-heading-note)
-       (define-key org-mode-map (kbd "C-t M-r") 'smishy-insert-new-heading-ref)
-
-       (define-key org-mode-map (kbd "C-t t") 'smishy-insert-heading-todo)
-       (define-key org-mode-map (kbd "C-t p") 'smishy-insert-heading-project)
-       (define-key org-mode-map (kbd "C-t n") 'smishy-insert-heading-note)
-       (define-key org-mode-map (kbd "C-t r") 'smishy-insert-heading-ref)
-
-       (define-key org-mode-map (kbd "C-t C-t") 'smishy-insert-subheading-todo)
-       (define-key org-mode-map (kbd "C-t C-p") 'smishy-insert-subheading-project)
-       (define-key org-mode-map (kbd "C-t C-n") 'smishy-insert-subheading-note)
-       (define-key org-mode-map (kbd "C-t C-r") 'smishy-insert-subheading-ref)
+       (define-key global-map (kbd "C-c c") 'org-capture)
 
        (define-key org-mode-map (kbd "C-t i") 'org-clock-in)
        (define-key org-mode-map (kbd "C-t o") 'org-clock-out)
@@ -76,24 +86,10 @@ Sets keys for org-mode-map and org-agenda-mode-map.
        (define-key org-mode-map (kbd "C-c C-t") 'org-set-tags)
 
        (define-key org-mode-map (kbd "C-c h") (lambda () (interactive) (org-agenda nil "h")))
-       (define-key org-mode-map (kbd "C-c t") (lambda () (interactive) (org-todo-list "TODO")))
+       (define-key org-mode-map (kbd "C-c t") (lambda () (interactive) (org-todo-list "ACTION")))
        (define-key org-mode-map (kbd "C-c n") (lambda () (interactive) (org-agenda-list 56)))
 
-      ;; (define-key org-mode-map (kbd "C-c C-c") 'smishy-insert-todo)
-      ;; (define-key org-mode-map (kbd "C-c C-d") 'org-deadline)
-      ;; (define-key org-mode-map (kbd "C-c C-h") 'org-schedule)
-
-      ;; (define-key org-mode-map (kbd "C-c C-l") 'org-store-link)
-
-      ;; (define-key org-mode-map (kbd "C-c e") 'smishy-reload-tasks)
-      ;; (define-key org-mode-map (kbd "C-c p") 'smishy-create-project)
-      ;; (define-key org-mode-map (kbd "C-c c") 'smishy-reload-top)
-      ;; (define-key org-mode-map (kbd "C-c a") 'org-agenda)
-
-
-
-      ;; (define-key org-mode-map (kbd "C-c s") 'smishy-save-n-go)
-      ;; (define-key org-mode-map (kbd "C-c b") 'org-iswitchb)
+      (define-key org-mode-map (kbd "C-c a") 'org-agenda)
 
       ;; The following keybinds are for when emacs is in an xterm, shift + direction
       ;; keys return ESC [ 1 ; 2 bla  for some reason (reason is found in ECMA-48) ;;
@@ -107,7 +103,7 @@ Sets keys for org-mode-map and org-agenda-mode-map.
       (define-key org-agenda-mode-map (kbd "TAB") 'smishy--tab-out-of-agenda))))
 
 (defun smishy--set-max-priority()
-  "Set the current TODO to have the maximum priority"
+  "Set the current ACTION to have the maximum priority"
   (let* ((marker (or (org-get-at-bol 'org-hd-marker)
                      (org-agenda-error)))
          (buffer (marker-buffer marker)))
--- smishy-taskflow.el
+++ smishy-taskflow.el
@@ -9,159 +9,22 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; public functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun smishy-reload-top ()
-  "Prepare new Next Action line.
-This function will jump to line smishy--work-line, newline it and then put a * NEXT ACTION line in and get ready for input."
-  (interactive)
-  ;; (switch-to-buffer "biglist.org")
-  (goto-line smishy--work-line)
-  (setq mystr (buffer-substring (point-at-bol) (point-at-eol)))
-  (cond ((string= mystr "* NEXT ACTION ") (move-end-of-line 1))
-        ((string= mystr "* NEXT ACTION")
-         (move-beginning-of-line 1)
-         (kill-line)
-         (insert "* NEXT ACTION "))
-        ((string= mystr "")
-         (move-beginning-of-line 1)
-         (kill-line)
-         (newline)
-         (goto-line smishy--work-line)
-         (insert "* NEXT ACTION "))
-        ((string-match "^ +$" mystr) ;tests for 1 or more blank spaces only
-         (move-beginning-of-line 1)
-         (kill-line)
-         (newline)
-         (goto-line smishy--work-line)
-         (insert "* NEXT ACTION "))
-        (t
-         (move-beginning-of-line 1)
-         (newline)
-         (goto-line smishy--work-line)
-         (insert "* NEXT ACTION ")))
-  (goto-line (+ 1 smishy--work-line))
-  (if (string-match "\* NEXT ACTION ." mystr)
-      (org-todo "TODO"))
-  (setq mystr2 (buffer-substring (point-at-bol) (point-at-eol)))
-  (goto-line (+ 2 smishy--work-line))
-  ;; This bit processes the 2nd line under the work line and turns it into a
-  ;; TODO which doesn't really seem to be a good thing
-  ;; (if (and (string-match "\* TODO " mystr2)
-  ;;          (string-match "\* NEXT ACTION ." mystr))
-  ;;     (org-todo "TODO"))
-  (goto-line smishy--work-line)
-  (move-end-of-line 1)
-  (save-buffer))
-
-(defun smishy-save-n-go ()
-  "Save new Next Action and clone frame.
-Push the current task down, add a new DOING, then save the whole file. Finally, detach the screen, thus killing the xterm."
-  (interactive)
-  (smishy-reload-top)
-  (save-buffer)
-  (shell-command "screen -D smishy--taskflow"))
-
 (defun smishy-toggle-done ()
-  "Toggle TODO/DONE on current line.
+  "Toggle ACTION/DONE on current line.
 Finish task on the current line and save it at the bottom as 'DONE'"
   (interactive)
   (setq mystr (buffer-substring (point-at-bol) (point-at-eol)))
-  (cond ((string-match "^\** TODO " mystr) (org-todo "DONE"))
-        ((string-match "^\** DONE " mystr) (org-todo "TODO"))))
+  (cond ((string-match "^\** ACTION " mystr) (org-todo "DONE"))
+        ((string-match "^\** DONE " mystr) (org-todo "ACTION"))))
 
 (defun smishy-create-project ()
   "Create an Org-Mode project.
-Creates an Org-Mode project, then allows you to insert a TODO"
+Creates an Org-Mode project, then allows you to insert a ACTION"
   (interactive)
   (if (equal (line-number-at-pos) smishy--work-line)
     (goto-line (+ smishy--work-line 1)))
   (org-insert-todo-heading nil)
   (org-todo "PROJECT"))
-
-(defun smishy-insert-heading-todo ()
-  "Insert TODO heading"
-  (interactive)
-  (smishy--insert-heading "TODO"))
-
-(defun smishy-insert-heading-project ()
-  "Insert PROJECT heading"
-  (interactive)
-  (smishy--insert-heading "PROJECT"))
-
-(defun smishy-insert-heading-ref ()
-  "Insert REF heading"
-  (interactive)
-  (smishy--insert-heading "REF"))
-
-(defun smishy-insert-heading-note ()
-  "Insert NOTE heading"
-  (interactive)
-  (smishy--insert-heading "NOTE"))
-
-(defun smishy-insert-new-heading-todo ()
-  "Insert TODO new-heading"
-  (interactive)
-  (smishy--insert-new-heading "TODO"))
-
-(defun smishy-insert-new-heading-project ()
-  "Insert PROJECT new-heading"
-  (interactive)
-  (smishy--insert-new-heading "PROJECT"))
-
-(defun smishy-insert-new-heading-ref ()
-  "Insert REF new-heading"
-  (interactive)
-  (smishy--insert-new-heading "REF"))
-
-(defun smishy-insert-new-heading-note ()
-  "Insert NOTE new-heading"
-  (interactive)
-  (smishy--insert-new-heading "NOTE"))
-
-(defun smishy-insert-subheading-todo ()
-  "Insert TODO subheading"
-  (interactive)
-  (smishy--insert-subheading "TODO"))
-
-(defun smishy-insert-subheading-project ()
-  "Insert PROJECT subheading"
-  (interactive)
-  (smishy--insert-subheading "PROJECT"))
-
-(defun smishy-insert-subheading-ref ()
-  "Insert REF subheading"
-  (interactive)
-  (smishy--insert-subheading "REF"))
-
-(defun smishy-insert-subheading-note ()
-  "Insert NOTE subheading"
-  (interactive)
-  (smishy--insert-subheading "NOTE"))
-
-(defun smishy--insert-heading (val)
-  "Insert val heading
-Creates a val heading in the current line"
-  ;; (move-end-of-line nil)
-  ;; (newline)
-  (org-insert-todo-heading nil)
-  (org-todo val))
-
-(defun smishy--insert-new-heading (val)
-  "Insert new val heading
-Creates a new val heading in the current line"
-  ;; (move-end-of-line nil)
-  ;; (newline)
-  (goto-line (+ 1 smishy--work-line))
-  (org-insert-todo-heading nil)
-  (org-todo val))
-
-(defun smishy--insert-subheading (val)
-  "Insert val subheading
-Creates a val subheading in the current line"
-  ;; (move-end-of-line nil)
-  ;; (newline)
-  (org-insert-todo-subheading nil)
-  (org-todo val))
-
 
 (defun smishy-start-taskflow (file-path)
   "Start the smishy task flow"
@@ -172,38 +35,12 @@ Creates a val subheading in the current line"
     (add-to-list 'org-agenda-files (buffer-file-name buff)))
   (smishy--set-variables)
   (smishy--set-faces)
+  (smishy--set-capture-templates)
   (smishy--set-key-bindings)
   ;; (smishy--auto-update-agenda-views-start)
   (delete-other-windows)
-  (smishy-reload-top)
   (org-mode)
   (org-agenda nil "h"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Test stuff, Ignore! ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun smishy--lock-spaces-down ()
-  "This function makes the entire document read only, except for a space on
-the work line. It is problematic as I still cant apply functions properly..."
-  (interactive)
-  ;; find the point that indicates the end of the 1st read only region
-  (goto-line smishy--work-line)
-  (setq mystr (buffer-substring (point-at-bol) (point-at-eol)))
-  (if (string-match "\* [A-Z]+ " mystr)
-      (progn
-        (setq readonly-region1-end (- (+ (point) (match-end 0)) 1)))
-    (setq readonly-region1-end nil))
-  ;; find the point that indicates the start of the 2nd read only region
-  (goto-line (+ smishy--work-line 1))
-  (setq readonly-region2-start (- (point) 1))
-  ;; set read only regions
-  (if readonly-region1-end
-      (add-text-properties (point-min) readonly-region1-end '(read-only t)))
-  (add-text-properties readonly-region2-start (point-max) '(read-only t)))
-
-(defun smishy-reload-tasks ()
-  "Function to reload .smishy--taskflow, used during devel"
-  (interactive)
-  (load-file "~/Code/smishy--taskflow/smishy--taskflow.el"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; Fancy colors, Ignore! ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -223,6 +60,3 @@ the work line. It is problematic as I still cant apply functions properly..."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Finished ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'smishy-taskflow)
-
-
-;; (setq org-agenda-start-with-clockreport-mode nil)
